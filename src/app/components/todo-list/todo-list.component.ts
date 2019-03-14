7167332f40d3f59a78e81d2b32dc2bc388c4c97d
Vasia <vasialeleka@ukr.net>
2019-03-14 16:12:34 +0200

    prepear fot editing

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,26 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import{ Todo } from '../../interfaces/todo';
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
-todos:object[];
+todos:Todo[];
 todoTitle:string;
 idForTodo: number;
   constructor() { }
 
   ngOnInit() {
     this.todoTitle ='';
-    this.todos = [{'id':1,
+    this.todos = [{
+    'id':1,
     'title':'First todo',
     'completed':false,
-    'edditing':false},
-    {'id':2,
+    'editing':false},
+    {
+    'id':2,
     'title':'Second todo',
     'completed':false,
-    'edditing':false}]
+    'editing':false}]
     this.idForTodo = this.todos.length;
   }
  addTodo():void{
@@ -29,7 +31,7 @@ idForTodo: number;
      id : this.idForTodo,
      title:this.todoTitle,
      completed:false,
-     edditing:false
+     editing:false
 
    })
    this.todoTitle="";
@@ -38,6 +40,15 @@ idForTodo: number;
 
  deleteTodo(id:number):void{
    this.todos = this.todos.filter(todo => todo.id !== id);
+ }
+
+ editTodo(todo:Todo):void{
+   todo.editing = true;
 
  }
+
 }
+
+
+
+
