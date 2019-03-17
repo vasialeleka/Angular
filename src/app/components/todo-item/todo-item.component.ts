d4a5a25075c92f0e9c1354cdc33505be034dc72b
Vasia <vasialeleka@ukr.net>
2019-03-17 13:49:49 +0200

    add events for component

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import{ Todo } from 'src/app/interfaces/todo';
 @Component({
   selector: 'todo-item',
@@ -7,9 +7,29 @@ import{ Todo } from 'src/app/interfaces/todo';
 })
 export class TodoItemComponent implements OnInit {
 @Input() todo:Todo;
+@Output() checkedItem = new EventEmitter();
+@Output() clickOnItem = new EventEmitter();
+@Output() blurItem = new EventEmitter();
+@Output() enterItem = new EventEmitter();
+@Output() deleteItem = new EventEmitter();
+@Output() cancelItem = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  doneEdit(todo:Todo):void{
+    this.checkedItem.emit(todo);
+  }
+  editTodo(todo:Todo):void{
+    this.clickOnItem.emit(todo);
+  }
+  cancelTodo(todo:Todo):void{
+    this.cancelItem.emit(todo);
+  }
+  deleteTodo(todo:Todo):void{
+    this.deleteItem.emit(todo);
   }
 
 }
